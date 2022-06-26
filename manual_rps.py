94943ad865e627c2fe5e45e45037543115a7d99d
shbz8888 <shbz8888@gmail.com>
2022-06-26 12:36:51 +0100

    first commit

---
+++ manual_rps.py
@@ -0,0 +1,21 @@
+import random
+
+    def get_computer_choice(choice):
+        choices = ["rock","paper","scissors"]
+        computer = random.choice(choices)
+        print(computer)
+    pass
+
+    def get_user_choice(input):
+        player = input("rock, paper, or scissors?: ")
+        print(input)
+    pass
+
+def play_game()
+
+
+play_again = input("Play again? (yes/no): ").lower()
+
+if play_again != "yes":
+    break
+
