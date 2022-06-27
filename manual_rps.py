d605b5d3dbd487df8f13cdce6e3e6280fcaa4e54
shbz8888 <shbz8888@gmail.com>
2022-06-27 12:11:36 +0100

    fourth commit, the get_winner function is somewhat opoerational

--- manual_rps.py
+++ manual_rps.py
@@ -1,29 +1,42 @@
 import random
-
 def get_winner(computer,player):
     if computer == player:
-        print('it\'s a tie')
-    elif (computer == 'rock' and player == 'scissors') or (computer == 'paper' and player == 'rock') or (computer == 'scissors' and player == 'paper'):
-        print('computer won')
+        print("it'\s a tie")
+    elif computer == 'rock':
+        if player == 'scissors':
+            return print("Computer won")
+        else:
+            return print("Player won")
+    elif computer == 'paper':
+        if player == 'rock':
+            return  print("Computer won")
+        else:
+            return print("Player won")
+    elif computer == 'scissors':
+        if player == 'paper':
+            return  print("Computer won")
+        else:
+            return print("Player won")
     else:
-        print('player won congratualtions')
-    
-
-
+        print('could not be processed')
+    return 
+   
 def get_computer_choice():
     choices = ["rock","paper","scissors"]
     computer = random.choice(choices)
     print(f'computer choice is {computer}')
     return computer
- ##%    
+    ##%    
+
 
 def get_user_choice():
     player = input('rock, paper, or scissors?: ').lower()
     return player
 
+        
 get_computer_choice()
 get_user_choice()
-
+get_winner(get_computer_choice(),get_user_choice())
 
 
     
