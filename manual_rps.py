3b2691d392130574aad9dbac1e2553946f05b403
shbz8888 <shbz8888@gmail.com>
2022-06-28 12:53:26 +0100

    code with comments made for easier code comprehension

--- manual_rps.py
+++ manual_rps.py
@@ -1,20 +1,20 @@
 import random
 
 
-   
+# computer randomly selects a choice   
 def get_computer_choice():
     choices = ["rock","paper","scissors"]
     computer = random.choice(choices)
     print(f'computer choice is {computer}')
     return computer
-# computer randomly selects a choice  
 
 
+# user is asked to input a choice  
 def get_user_choice():
     player = input('rock, paper, or scissors?: ').lower()
     return player
-# user is asked to input a choice  
-
+ 
+# The winner is decided after the computer and player have made their decisions   
 def get_winner(computer,player):
     if computer == player:
         print("it's a tie")
@@ -33,14 +33,14 @@ def get_winner(computer,player):
     else:
         print('Try again')
     return 
-# The winner is decided after the computer and player have made their decisions   
+ 
 
-
+# A play function to run the game
 def play():
     computer = get_computer_choice()
     player = get_user_choice()
     winner = get_winner(computer,player)
     return
-# A play function to run the game
-play()
 #  The play function is called
+play()
+
